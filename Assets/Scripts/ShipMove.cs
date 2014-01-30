e21cd53e90df3c036b39af52e06aa43c1f244d0e
Rohun Banerji <rohunb@gmail.com>
2014-01-29 22:00:26 -0500

    started project

---
+++ Assets/Scripts/ShipMove.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMove : MonoBehaviour {
+
+    public GameObject[] forwardThrusters;
+    public GameObject[] reverseThrusters;
+
+    private ThrusterForce thruster;
+    // Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float moveV = Input.GetAxis("Vertical");
+        float moveH = Input.GetAxis("Horizontal");
+        if(moveV>0.0f)
+        {
+            FireForwardThrusters();
+
+        }
+        else if(moveV<=0.0f)
+        {
+            FireReverseThrusters();
+        }
+        if (moveH > 0.0f)
+        {
+            FireStrafeRightThrusters();
+
+        }
+        else if (moveH <= 0.0f)
+        {
+            FireStrafeLeftThrusters();
+        }
+	}
+
+    void FireForwardThrusters()
+    {
+        for (int i = 0; i < forwardThrusters.Length; i++)
+        {
+            thruster = forwardThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+        
+    }
+    void FireReverseThrusters()
+    {
+
+    }
+    void FireStrafeRightThrusters()
+    {
+
+    }
+    void FireStrafeLeftThrusters()
+    {
+
+    }
+
+}
+
---
+++ Assets/Scripts/ThrusterForce.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrusterForce : MonoBehaviour {
+
+    public float maxThrustForce = 100.0f;
+    public bool damaged;
+    private Rigidbody spaceship;
+	// Use this for initialization
+	void Start () {
+        damaged = false;
+        spaceship = transform.parent.gameObject.rigidbody;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+	}
+    public void FireThruster()
+    {
+        Debug.Log("firing thruster");
+        Vector3 direction = spaceship.transform.position - transform.position;
+        spaceship.AddForceAtPosition(direction.normalized, transform.position);
+        //spaceship.AddForce(transform.forward * maxThrustForce);
+
+    }
+}
