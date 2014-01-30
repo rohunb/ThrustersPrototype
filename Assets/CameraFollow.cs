3bec5f6b923efb0003fdda6d32039bc11f150161
Rohun Banerji <rohunb@gmail.com>
2014-01-30 13:44:08 -0500

    added 2d camera follow

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public Transform target;
+    //public float smoothTime = 0.3F;
+    
+
+    private Vector2 Velocity;
+    void Update()
+    {
+        //float newPositionX = Mathf.SmoothDamp(transform.position.x, target.position.x, ref Velocity.x, smoothTime);
+        //float newPositionY = Mathf.SmoothDamp(transform.position.y, target.position.y, ref Velocity.y, smoothTime);
+
+        transform.position = new Vector3(target.position.x + 6f, transform.position.y, transform.position.z);
+    }
+}
