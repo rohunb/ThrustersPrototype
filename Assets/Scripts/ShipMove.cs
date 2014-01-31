8828536dff68839ac3fc76e398f37dc33c9d642b
Rohun Banerji <rohunb@gmail.com>
2014-01-30 23:46:48 -0500

    vertical controls

--- Assets/Scripts/ShipMove.cs
+++ Assets/Scripts/ShipMove.cs
@@ -10,6 +10,19 @@ public class ShipMove : MonoBehaviour {
     public GameObject[] rearLeftThrusters;
     public GameObject[] rearRightThrusters;
 
+    //new
+    public GameObject[] bottomLeftThrusters;
+    public GameObject[] bottomRightThrusters;
+    public GameObject[] topLeftThrusters;
+    public GameObject[] topRightThrusters;
+    public GameObject[] bottomRearThrusters;
+    public GameObject[] bottomFwdThrusters;
+    public GameObject[] topRearThrusters;
+    public GameObject[] topFwdThrusters;
+    //new
+
+
+
     private ThrusterForce thruster;
     // Use this for initialization
 	void Start () {
@@ -48,7 +61,68 @@ public class ShipMove : MonoBehaviour {
         }
         if (Input.GetKey(KeyCode.Space))
             Stabilize();
+
+        //new
+        //move up
+        if (Input.GetKey(KeyCode.U))
+            MoveUp();
+        if (Input.GetKey(KeyCode.H))
+            MoveDown();
+        if(Input.GetKey(KeyCode.J))
+            RollLeft();
+        if(Input.GetKey(KeyCode.L))
+            RollRight();
+        if(Input.GetKey(KeyCode.I))
+            PitchForward();
+        if(Input.GetKey(KeyCode.K))
+            PitchBack();
+
+
+
+
+        //new
 	}
+
+    //new
+    void MoveUp()
+    {
+        FireBottomThrusters();
+    }
+    void MoveDown()
+    {
+        FireTopThrusters();
+    }
+    void RollLeft()
+    {
+        FireBottomRightThrusters();
+        FireTopLeftThrusters();
+    }
+
+    
+    void RollRight()
+    {
+        FireBottomLeftThrusters();
+        FireTopRightThrusters();
+    }
+
+    
+    void PitchForward()
+    {
+        FireBottomRearThrusters();
+        FireTopFwdThrusters();
+    }
+
+   
+    void PitchBack()
+    {
+        FireBottomFwdThrusters();
+        FireTopRearThrusters();
+    }
+
+    
+    //new
+
+
 
     void FireForwardThrusters()
     {
@@ -155,5 +229,153 @@ public class ShipMove : MonoBehaviour {
             Mathf.Lerp(aVel.z, 0f, Time.deltaTime));
 
     }
+
+    //new
+
+    /*
+     * public GameObject[] bottomLeftThrusters;
+    public GameObject[] bottomRightThrusters;
+    public GameObject[] topLeftThrusters;
+    public GameObject[] topRightThrusters;
+    public GameObject[] bottomRearThrusters;
+    public GameObject[] bottomFwdThrusters;
+    public GameObject[] topRearThrusters;
+    public GameObject[] topFwdThrusters;
+     * */
+     
+    
+    void FireBottomThrusters()
+    {
+        for (int i = 0; i < bottomRearThrusters.Length; i++)
+        {
+            thruster = bottomRearThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+        for (int i = 0; i < bottomFwdThrusters.Length; i++)
+        {
+            thruster = bottomFwdThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+    void FireTopThrusters()
+    {
+        for (int i = 0; i < topRearThrusters.Length; i++)
+        {
+            thruster = topRearThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+        for (int i = 0; i < topFwdThrusters.Length; i++)
+        {
+            thruster = topFwdThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+    private void FireTopLeftThrusters()
+    {
+        for (int i = 0; i < topLeftThrusters.Length; i++)
+        {
+            thruster = topLeftThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+
+    private void FireBottomRightThrusters()
+    {
+        for (int i = 0; i < bottomRightThrusters.Length; i++)
+        {
+            thruster = bottomRightThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+    private void FireTopRightThrusters()
+    {
+        for (int i = 0; i < topRightThrusters.Length; i++)
+        {
+            thruster = topRightThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+
+    private void FireBottomLeftThrusters()
+    {
+        for (int i = 0; i < bottomLeftThrusters.Length; i++)
+        {
+            thruster = bottomLeftThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+    private void FireTopFwdThrusters()
+    {
+        for (int i = 0; i < topFwdThrusters.Length; i++)
+        {
+            thruster = topFwdThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+
+    private void FireBottomRearThrusters()
+    {
+        for (int i = 0; i < bottomRearThrusters.Length; i++)
+        {
+            thruster = bottomRearThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+    private void FireTopRearThrusters()
+    {
+        
+        for (int i = 0; i < topRearThrusters.Length; i++)
+        {
+            thruster = topRearThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+
+    private void FireBottomFwdThrusters()
+    {
+        for (int i = 0; i < bottomFwdThrusters.Length; i++)
+        {
+            thruster = bottomFwdThrusters[i].GetComponent<ThrusterForce>();
+            if (!thruster.damaged)
+            {
+                thruster.FireThruster();
+            }
+        }
+    }
+
+    //new
 }
 
