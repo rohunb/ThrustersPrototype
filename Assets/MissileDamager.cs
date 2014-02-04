71aa286010e945c7141597235cdcf5f2ada5ce7e
Rohun Banerji <rohunb@gmail.com>
2014-02-04 13:37:38 -0500

    added missile explosion

---
+++ Assets/MissileDamager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileDamager : MonoBehaviour {
+    public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void OnTriggerEnter(Collider other)
+    {
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
