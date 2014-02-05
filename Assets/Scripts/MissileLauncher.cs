e73bb5ebb33512f93e3301936cc6283ab49372ab
Rohun Banerji <rohunb@gmail.com>
2014-02-05 15:26:24 -0500

    updated missile behaviour

--- Assets/Scripts/MissileLauncher.cs
+++ Assets/Scripts/MissileLauncher.cs
@@ -10,11 +10,15 @@ public class MissileLauncher : MonoBehaviour {
     public void FireMissile(float damage, Transform _target)
     {
         GameObject missileClone = Instantiate(missile, shootPoint.position, shootPoint.rotation) as GameObject;
-        missile.GetComponent<HomingMissile>().target = _target;
+
+        missileClone.rigidbody.AddForce(transform.up * -100f, ForceMode.Impulse);
+        missileClone.GetComponent<HomingMissile>().target = _target;
     }
     public void FireClusterMissile(float damage, Transform _target)
     {
         GameObject clusterMissileClone = Instantiate(clusterMissile, shootPoint.position, shootPoint.rotation) as GameObject;
-        clusterMissile.GetComponent<ClusterMissile>().target = _target;
+        clusterMissileClone.rigidbody.AddForce(transform.up * -50f, ForceMode.Impulse);
+        clusterMissileClone.GetComponent<HomingMissile>().target = _target;
+        clusterMissileClone.GetComponent<ClusterMissile>().target = _target;
     }
 }
