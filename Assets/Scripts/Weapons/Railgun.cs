17793b6e3d6649ab94457fe1a5241a8ef7c625b6
Rohun Banerji <rohunb@gmail.com>
2014-02-28 23:57:45 -0500

    minor updates

--- Assets/Scripts/Weapons/Railgun.cs
+++ Assets/Scripts/Weapons/Railgun.cs
@@ -35,20 +35,14 @@ public class Railgun : MonoBehaviour
         line.enabled = true;
         line.SetVertexCount(length);
         line.SetColors(lineStartColour, lineStartColour);
-        //Debug.Log(shootPoint.right);
-        
         for (int i = 0; i < length; i++)
         {
             Vector3 newPos=shootPoint.position;
-            
-            //newPos.x+=i * 0.5F;
-            newPos.x += 2f*Mathf.Cos(i + Time.time);
-            newPos.y+= 2f*Mathf.Sin(i + Time.time);
-            //newPos.z += i;
+            newPos.x += 2f * Mathf.Cos(i + Time.time);
+            newPos.y += 2f * Mathf.Sin(i + Time.time);
             newPos += shootPoint.forward * i;
             line.SetPosition(i, newPos);
         }
-        //Invoke("CancelRailEffect", .2f);
         lineColour = lineStartColour;
         clearEffect = true;
     }
@@ -59,10 +53,8 @@ public class Railgun : MonoBehaviour
             lineColour=Color.Lerp(lineColour, new Color(lineColour.r, lineColour.g, lineColour.b, 0.0f), Time.deltaTime * railEffectClearTimer);
             if (lineColour.a < 0.1f)
             {
-                //lineColour.a = 1.0f;
                 line.enabled = false;
             }
-            //line.renderer.material.color = lineColour;
             line.SetColors(lineColour,lineColour);
         }
         
@@ -72,28 +64,3 @@ public class Railgun : MonoBehaviour
         line.enabled = false;
     }
 }
-//public class Example : MonoBehaviour
-//{
-//    public Color c1 = Color.yellow;
-//    public Color c2 = Color.red;
-//    public int lengthOfLineRenderer = 20;
-//    void Start()
-//    {
-//        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
-//        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
-//        lineRenderer.SetColors(c1, c2);
-//        lineRenderer.SetWidth(0.2F, 0.2F);
-//        lineRenderer.SetVertexCount(lengthOfLineRenderer);
-//    }
-//    void Update()
-//    {
-//        LineRenderer lineRenderer = GetComponent<LineRenderer>();
-//        int i = 0;
-//        while (i < lengthOfLineRenderer)
-//        {
-//            Vector3 pos = new Vector3(i * 0.5F, Mathf.Sin(i + Time.time), 0);
-//            lineRenderer.SetPosition(i, pos);
-//            i++;
-//        }
-//    }
-//}
