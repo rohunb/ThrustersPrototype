8931f26e0954bf64208e19f6c6058dfec4680adc
Kyle Nokes <kyle_nokes@live.com>
2014-03-05 15:03:12 -0500

    Planet Textures and Materials

---
+++ Assets/Scripts/General/Clouds.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clouds : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    void Update()
+    {
+        renderer.material.SetTextureOffset("_MainTex", new Vector2(Time.time * scrollSpeed, 0));
+    }
+}
+
+//Kyle Nokes
