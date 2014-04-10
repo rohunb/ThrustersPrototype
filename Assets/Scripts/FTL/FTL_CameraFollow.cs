f14a7d4e3abcb6da8cc8f555d6fef8ea497854c9
Rohun Banerji <rohunb@gmail.com>
2014-04-09 22:46:35 -0400

    camera can no longer go off galaxy map in FTL scene

--- Assets/Scripts/FTL/FTL_CameraFollow.cs
+++ Assets/Scripts/FTL/FTL_CameraFollow.cs
@@ -8,21 +8,37 @@ public class FTL_CameraFollow : MonoBehaviour
 	private Camera ftlCamera;
 	private float targetCameraSize;
 	private float camZoomAmount = 10f;
+    
+    public int galaxyLayer = 12;
+    public Vector4 bounds;
+    Rect boundaries;
+
+
 	// Use this for initialization
 	void Start () 
 	{
 		ftlSpaceship = GameObject.Find ("Spaceship").transform;
 		ftlCamera = Camera.main;
 		targetCameraSize = ftlCamera.orthographicSize;
+        boundaries = new Rect();
+        boundaries.xMin = bounds.x;
+        boundaries.xMax = bounds.y;
+        boundaries.yMin = bounds.z;
+        boundaries.yMax = bounds.w;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		Vector3 targetCamPosition = new Vector3(ftlSpaceship.position.x, transform.position.y, ftlSpaceship.position.z);
+        if (CheckIfBeyondEdge())
+        {
+            Vector3 targetCamPosition = new Vector3(ftlSpaceship.position.x, transform.position.y, ftlSpaceship.position.z);
 
-		transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetCamPosition.x, Time.deltaTime * lerpSpeed),transform.position.y,Mathf.Lerp(transform.position.z, targetCamPosition.z, Time.deltaTime * lerpSpeed)); 
-
+            transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetCamPosition.x, Time.deltaTime * lerpSpeed), transform.position.y, Mathf.Lerp(transform.position.z, targetCamPosition.z, Time.deltaTime * lerpSpeed));
+        }
+        //float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+        //transform.Translate(new Vector3(h * 30 * Time.deltaTime, v * 30 * Time.deltaTime, 0.0f));
 
 		if (Input.GetAxis ("Mouse ScrollWheel") > 0.05f)
 		{
@@ -42,5 +58,71 @@ public class FTL_CameraFollow : MonoBehaviour
 
 		}
 		ftlCamera.orthographicSize = Mathf.Lerp(ftlCamera.orthographicSize, targetCameraSize, Time.deltaTime * lerpSpeed);
+
+        CheckIfBeyondEdge();
 	}
+    bool CheckIfBeyondEdge()
+    {
+        bool canMove = true;
+        
+        Ray topLeft = Camera.main.ViewportPointToRay(new Vector3(0, 1, 0));
+        Ray topRight = Camera.main.ScreenPointToRay(new Vector3(Screen.width, Screen.height - 1, 0));
+        Ray botLeft = Camera.main.ViewportPointToRay(new Vector3(0, 0, 0));
+
+        float left, right, top, bottom;
+        RaycastHit hit;
+
+        if (Physics.Raycast(topLeft, out hit, Mathf.Infinity, 1 << galaxyLayer))
+        {
+            left = hit.point.x;
+            top = hit.point.z;
+        }
+        else
+        {
+            transform.Translate(new Vector3(1.0f, 0f, 0f));
+            return false;
+        }
+
+        if(Physics.Raycast(topRight, out hit, Mathf.Infinity, 1 << galaxyLayer))
+        {right = hit.point.x;
+        }
+        else
+        {
+            transform.Translate(new Vector3(-1.0f, 0f, 0f));
+            return false;
+        }
+
+        Physics.Raycast(botLeft, out hit, Mathf.Infinity, 1 << galaxyLayer);
+        bottom = hit.point.z;
+
+        //Debug.Log("left right top, botton: " + new Vector4(left, right, top, bottom));
+
+        if (left < boundaries.xMin)
+        {
+            transform.Translate(new Vector3(boundaries.xMin - left, 0f, 0f));
+            canMove = false;
+            //Debug.Log("Left over");
+        }
+        else if (right > boundaries.xMax)
+        {
+            transform.Translate(new Vector3(boundaries.xMax - right, 0, 0), Space.World);
+            canMove = false;
+            //Debug.Log("right over");
+        }
+
+        if (bottom < boundaries.yMin)
+        {
+            transform.Translate(new Vector3(0, 0, boundaries.yMin - bottom), Space.World);
+            canMove = false;
+            //Debug.Log("bottom over");
+        }
+        else if (top > boundaries.yMax)
+        {
+            transform.Translate(new Vector3(0, 0, boundaries.yMax - top), Space.World);
+            canMove = false;
+            // Debug.Log("Top over");
+        }
+        return canMove;
+    }
+
 }
--- Assets/Scripts/FTL/FTL_GalaxyInfo.cs
+++ Assets/Scripts/FTL/FTL_GalaxyInfo.cs
@@ -148,7 +148,7 @@ public class FTL_GalaxyInfo : MonoBehaviour
         x /= 2.0f;
         y /= 2.0f;
         float avg = (x + y) / 2;
-        scaleConversionFactor = 35.0f / avg;
+        scaleConversionFactor = 100.0f / avg;
 
         for (int i = 0; i < numStars; i++)
         {
