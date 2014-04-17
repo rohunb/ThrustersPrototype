b8bea7a5291fe88d0ce9e07f2e7684e54e739472
Christan Adao <adaoc01@gmail.com>
2014-04-17 14:25:01 -0400

    Changes to MainMenu
    -skin for mainmenu and gamescene is now separate
    -changed the sized on buttons

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -7,7 +7,6 @@ public class MainMenu : MonoBehaviour {
     bool showCredits = false;
     bool showQuit = false;
 
-    bool fadeToGameScene = false;
     bool controlsInverted = false;
     float musicVolume = .75f;
     float soundVolume = .75f;
@@ -32,17 +31,11 @@ public class MainMenu : MonoBehaviour {
 
         if (showMainMenu)
         {
-            if (GUI.Button(new Rect(Screen.width * .05f, Screen.height * .24f, Screen.width * .2f, Screen.height * .06f), "New Game"))
+            if (GUI.Button(new Rect(Screen.width * .05f, Screen.height * .34f, Screen.width * .2f, Screen.height * .06f), "Play"))
             {
                 GOD.audioengine.playSFX("MenuPlayBtn");
                 showMainMenu = false;
                 iTween.FadeTo(GameObject.Find("Screen Fader"), iTween.Hash("alpha", 1, "time", .5, "onComplete", "LoadGameScene", "onCompleteTarget", gameObject));
-               // fadeToGameScene = true;
-            }
-            if (GUI.Button(new Rect(Screen.width * .05f, Screen.height * .34f, Screen.width * .2f, Screen.height * .06f), "Continue"))
-            {
-                GOD.audioengine.playSFX("TerminalBtn");
-                //showMainMenu = false;
             }
             if (GUI.Button(new Rect(Screen.width * .05f, Screen.height * .44f, Screen.width * .2f, Screen.height * .06f), "Options"))
             {
@@ -66,21 +59,21 @@ public class MainMenu : MonoBehaviour {
 
         if (showOptions)
         {
-            Rect optionsRect = new Rect(Screen.width / 2 - 150, Screen.height / 2 - 100, 300, 300);
+            Rect optionsRect = new Rect(Screen.width / 2 - 150, Screen.height / 2 - 100, 300, 260);
 
             GUI.Box(optionsRect, "Options");
             GUI.BeginGroup(optionsRect);
 
-            GUI.Label(new Rect(optionsRect.width / 2 - 50, 50, 100, 20), "Music Volume");
+            GUI.Label(new Rect(optionsRect.width / 2 - 65, 50, 200, 50), "Music Volume");
             musicVolume = GUI.HorizontalSlider(new Rect(optionsRect.width / 2 - 65, 70, 130, 20), musicVolume, 0, 1);
 
-            GUI.Label(new Rect(optionsRect.width / 2 - 50, 90, 100, 20), "Sound Volume");
+            GUI.Label(new Rect(optionsRect.width / 2 - 70, 90, 200, 50), "Sound Volume");
             soundVolume = GUI.HorizontalSlider(new Rect(optionsRect.width / 2 - 65, 110, 130, 20), soundVolume, 0, 1);
 
-            GUI.Label(new Rect(optionsRect.width / 2 - 50, 130, 100, 20), "Ship Volume");
+            GUI.Label(new Rect(optionsRect.width / 2 - 60, 130, 200, 50), "Ship Volume");
             shipVolume = GUI.HorizontalSlider(new Rect(optionsRect.width / 2 - 65, 150, 130, 20), shipVolume, 0, 1);
 
-            if (GUI.Button(new Rect(optionsRect.width / 2 - 150, optionsRect.height - 100, 150, 40), "Confirm"))
+            if (GUI.Button(new Rect(optionsRect.width / 2 - 150, optionsRect.height - 60, 150, 40), "Confirm"))
             {
                 GOD.audioengine.playSFX("TerminalBtnYes");
                 showOptions = false;
@@ -89,7 +82,7 @@ public class MainMenu : MonoBehaviour {
                 //save changes
                 ConfirmChanges();
             }
-            if (GUI.Button(new Rect(optionsRect.width / 2, optionsRect.height - 100, 150, 40), "Cancel"))
+            if (GUI.Button(new Rect(optionsRect.width / 2, optionsRect.height - 60, 150, 40), "Cancel"))
             {
                 GOD.audioengine.playSFX("TerminalBtn");
                 showOptions = false;
@@ -103,19 +96,19 @@ public class MainMenu : MonoBehaviour {
 
         if (showCredits)
         {
-            Rect creditsRect = new Rect(Screen.width / 2 - 300, Screen.height / 2 - 200, 600, 400);
+            Rect creditsRect = new Rect(Screen.width / 2 - 150, Screen.height / 2 - 200, 300, 400);
 
             GUI.Box(creditsRect, "Credits");
             GUI.BeginGroup(creditsRect);
 
-            GUI.Label(new Rect(10, 30, 100, 30), "McCann, Alex");
-            GUI.Label(new Rect(10, 60, 100, 30), "Banerji, Rohun");
-            GUI.Label(new Rect(10, 90, 100, 30), "Vo, David");
-            GUI.Label(new Rect(10, 120, 100, 30), "Powers, Geordie");
-            GUI.Label(new Rect(10, 150, 100, 30), "Nokes, Kyle");
-            GUI.Label(new Rect(10, 180, 100, 30), "Adao, Christian");
+            string[] names = new string[] { "McCann, Alex", "Banerji, Rohun", "Vo, David", "Powers, Geordie", "Nokes, Kyle", "Adao, Christian" };
 
-            if (GUI.Button(new Rect(creditsRect.width / 2 - 25, creditsRect.height - 25, 50, 20), "Return"))
+            for (int i = 0; i < names.Length; i++)
+            {
+                GUI.Label(new Rect(creditsRect.width / 2 - 80, 50 + 40 * i, 200, 40), names[i]);
+            }
+
+            if (GUI.Button(new Rect(creditsRect.width / 2 - 100, creditsRect.height - 80, 200, 70), "Return"))
             {
                 GOD.audioengine.playSFX("TerminalBtn");
                 showCredits = false;
@@ -126,21 +119,22 @@ public class MainMenu : MonoBehaviour {
 
         if (showQuit)
         {
-            Rect quitRect = new Rect(Screen.width / 2 - 50, Screen.height / 2 - 20, 100, 60);
+            Rect quitRect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 20, 200, 100);
 
             GUI.Box(quitRect, "Quit?");
             GUI.BeginGroup(quitRect);
-            if (GUI.Button(new Rect(5, quitRect.height - 25, 45, 20), "Yes"))
+            if (GUI.Button(new Rect(0, quitRect.height - 45, 100, 40), "Yes"))
             {
                 GOD.audioengine.playSFX("TerminalBtnYes");
                 Application.Quit();
             }
-            if (GUI.Button(new Rect(quitRect.width - 50, quitRect.height - 25, 45 ,20), "No"))
+            if (GUI.Button(new Rect(quitRect.width / 2, quitRect.height - 45, 100 , 40), "No"))
             {
                 GOD.audioengine.playSFX("TerminalBtn");
                 showQuit = false;
                 showMainMenu = true;
             }
+
             GUI.EndGroup();
         }
     }
