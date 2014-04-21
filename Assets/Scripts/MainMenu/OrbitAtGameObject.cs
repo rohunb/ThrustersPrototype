d12dfba76f7a9aad4a6b4fc22dc330d8fb681e36
Christan Adao <adaoc01@gmail.com>
2014-04-21 18:54:49 -0400

    Merge branch 'master' of https://github.com/rohunb/ThrustersPrototype
    Conflicts:
    	ThrustersPrototype-csharp.v12.suo
    	obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

--- Assets/Scripts/MainMenu/OrbitAtGameObject.cs
+++ Assets/Scripts/MainMenu/OrbitAtGameObject.cs
@@ -4,11 +4,9 @@ using System.Collections;
 public class OrbitAtGameObject : MonoBehaviour {
     public GameObject targetObject;
     public float orbitSpd;
-    public GameObject explosion;
 
 	// Use this for initialization
 	void Start () {
-        ShakeCamera();
 	}
 	
 	// Update is called once per frame
@@ -16,14 +14,4 @@ public class OrbitAtGameObject : MonoBehaviour {
         gameObject.transform.LookAt(targetObject.transform);
         gameObject.transform.RotateAround(Vector3.zero, Vector3.up, orbitSpd * Time.deltaTime);
 	}
-
-    void ShakeCamera()
-    {
-        iTween.ShakePosition(gameObject, iTween.Hash("amount", new Vector3(.2f, .2f, .2f), "delay", 4, "onComplete", "ShakeCamera", "islocal", true, "onStart", "AddExplosions"));
-    }
-
-    void AddExplosions()
-    {
-        Instantiate(explosion, new Vector3(0, 7.75f, 0), Quaternion.identity);
-    }
 }
